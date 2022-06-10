570f8c49e39ecb37cc19c215b3cf5bc6d6c283f7
Michele Bisceglia <mikael_bisceglia@libero.it>
2022-06-10 18:03:21 +0200

    Milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include "database.php";?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,16 +10,28 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
   <title>Document</title>
 </head>
-  <header>
-    <div>
-      <img src="img/logo-small.svg" alt="">
-    </div>
-  </header>
-
-  <main>
-
-  </main>
 <body>
-  
+  <header>
+      <div>
+        <img src="img/logo-small.svg" alt="">
+      </div>
+    </header>
+
+    <main class="text-light">
+      <div class="container pt-5">
+        <div class="row">
+          <?php 
+          foreach($database as $item) {
+        echo '<div class="col-2 my_card">' ; 
+          echo '<img class="w-50 m-5" src="' . $item['poster'] . '">' ;
+          echo '<p>' . $item['title'] . '</p>' ;
+          echo '<p>' . $item['author']; '</p>' ;
+          echo '<p>' . $item['year']; '</p>' ;
+          echo '<p>' . $item['genre']; '</p>' ;
+        echo '</div>';
+        } ?>
+        </div>
+      </div>
+    </main>
 </body>
 </html>
